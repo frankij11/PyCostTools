b7aef90f1a5e7f700337f423a374112e3d67087e
frankij11 <kevinfjoy@gmail.com>
2020-09-28 15:28:29 -0400

    Update learn.py to be compatible with pandas versions

--- pycost/learn.py
+++ pycost/learn.py
@@ -19,10 +19,14 @@ def lc(T1, LC, RC, Qty, Rate):
 
 def lc_prep(df, cols, val= "value"):
 
-    lc =df.groupby(cols)[val].agg(share_qty = 'sum').reset_index()
+    if pd.__version__ >= '1.0':
+        lc =df.groupby(cols)[val].agg(share_qty = 'sum').reset_index()
+    else:
+        lc = df.groupby(cols)[val].agg(sum).reset_index()
+        lc = lc.rename(columns={val:"share_qty"})
     lc["Last"] = lc.groupby(cols[:-1])["share_qty"].cumsum()
     lc["First"] = lc["Last"] - lc["share_qty"] + 1
     lc["midpoint"] = lc_midpoint(lc["First"], lc["Last"])
-    
+    lc = lc[cols+ ['First', 'Last', 'midpoint', 'share_qty']]
     lc = pd.merge(df,lc,how='left', on=cols,sort=False)
     return lc
