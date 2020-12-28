c349c46f69cba41891d0cb6468ff5c0fd57f4386
frankij11 <kevinfjoy@gmail.com>
2020-12-27 20:58:35 -0500

    Update clean.py

--- pycost/clean.py
+++ pycost/clean.py
@@ -1,61 +1,74 @@
-def to_numeric(df, exclude=list(), **kwds):
-    for col in df.select_dtypes(exclude=np.number).columns:
-        if (True in df[col]) & (False in df[col]):
-            #df[col] = df[col].astype('bool')
+import pandas as pd
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class CategoricalImputer(BaseEstimator, TransformerMixin):
+    def __init__(self, variables=None):
+    #Check if the variables passed are in a list format, if not convert 
+    #to list format and assign it to self.variables to be used in later 
+    #methods
+        if not isinstance(variables,list):
+            self.variables = [variables]
         else:
+            self.variables = variables
+    
+    def fit(self, X:pd.DataFrame,y:pd.Series=None):
+        #Nothing to do here, just return the dataframe as is
+        return self
+    
+    def transform(self, X:pd.DataFrame):
+	      #Fill missing values and return the modified dataframe
+        X=X.copy()
+        for feature in self.variables:
+            X[feature] = X[feature].fillna("Missing")
+        return X
+
+class Clean(BaseEstimator, TransformerMixin):
+    def __init__(self,n_cats_max=20, drop_cols=list()):
+        self.n_cats_max = cats_max
+        if type(drop_cols) != list: drop_cols=[drop_cols]
+        self.drop_cols = drop_cols
+        
+        
+
+    def fit(self, X:pd.DataFrame, y:pd.Series=None):
+        df = pd.concat([X,y], axis=1)
+        df.columns = list(X.columns) + ["y"]
+        self.cat_vars = X.select_dtypes(include='object').columns.tolist()
+        self.num_vars = X.select_dtypes(include=np.number).columns.tolist()
+    
+        self.cats_ = dict()
+        
+        for var in self.cat_vars:
+            top_vals = X.groupby([var])["y"].sum().sort_values(
+                ascending=False).reset_index()
+            top_vals = top_vals.loc[range(0,min(n_cats_max+1, len(top_vals))),var].columns.tolist()     
+            self.cats_[var] = top_vals
+            
+        return self
+
+    def transform(self,X:pd.DataFrame):
+        X = X.copy()
+        # Fill categorical values that are missing
+        # Fill categorical values not in original dataset
+        for feature in self.cat_vars:
+            X[feature] = X[feature].fillna("Other")
+            new_cat = ~X[feature].isin(self.cats_[var].value)
+            X[feature][new_cat] = "Other"
+
+        X=pd.get_dummies(X)
+
+
+        # Clean Numerical Data
+        # Not implemented
+        for var in self.num_vars:
+            X[var] = pd.to_numeric(X[var], errors='coerce')
+        
+        # Drop Columns
+        for col in self.drop_cols:
             try:
-                res = df[col].str.replace("$", "").str.replace(" ", "")
-                df[col] = pd.to_numeric(res)
+                X.drop(columns=col, inplace=True)
             except:
                 pass
-        return df
-        
-def to_category(df, thresh=.05, include = list(),**kwds):
-    for col in df.select_dtypes(include='object'):
-        ratio = len(df[col].value_counts()) / len(df)
-        if ratio < thresh:
-            df[col] = df[col].astype('category')
 
-    for col in include:
-        try:
-            df[col] = df[col].astype('category')
-        except:
-            pass
-
-    return df
-
-def drop_cols(df, na_thresh=.6, drop_cols=list(), unique_thresh = .05, unique_n=25, cv=.05, **kwds):
-    # drop columns with mostly missing
-    thresh = len(df) * na_thresh
-    df.dropna(axis=1, thresh=thresh, inplace=True)
-
-    # drop columns with lots of values
-    for col in df.select_dtypes(exclude=np.number):
-        ratio = len(df[col].unique()) / len(df)
-        if (ratio > thresh) | (len(df[col].unique()) ==1) | (len(df[col].unique()) > unique_n):
-            df.drop(columns = col, inplace=True)
-
-
-    # drop low variance columns
-    for col in df.select_dtypes(include=np.number):
-        cv = df[col].mean(skipna=[np.nan,None])/ df[col].std(skipna=[np.nan,None]) 
-
-    # drop user specificed columns
-    for col in drop_cols:
-        try:
-            df.drop(columns=col, inplace=True)
-        except:
-            pass
-
-    
-    return df
-
-def clean_df(df,**kwds):
-    df_copy = df.copy()
-    df_copy =(df_copy.
-               pipe(to_numeric, ).
-               pipe(to_category, ).
-               pipe(drop_cols, **kwds)
-               )
-    return df_copy
-
+        return X
