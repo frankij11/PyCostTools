3fa6b25bd85cf6da6450ceff6e27359b0a0f65c4
frankij11 <kevinfjoy@gmail.com>
2021-02-21 14:05:36 -0500

    clean up git repository

--- build/lib/pycost/__init__.py
+++
@@ -1,9 +0,0 @@
-#import pkg_resources
-#my_data = pkg_resources.resource_string(__name__, "data/inflation.csv")
-#print(my_data)
-
-
-from .utils import *
-from .inflation import *
-from .learn import *
-
--- build/lib/pycost/utils.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-def get_fys(df):
-    fys = df.columns.str.replace("FY", "").str.strip().str.isdigit()
-    return fys
-
-def stack_fys(df, fy_name="FY"):
-    fys = get_fys(df)
-    df_stacked = pd.melt(df, id_vars=df.columns[~fys], value_vars=df.columns[fys], var_name=fy_name)
-    return df_stacked
